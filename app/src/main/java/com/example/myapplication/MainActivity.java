231cb36c86fe2b636ddb42d868c0198ed9babd01
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2022-04-14 20:42:03 +0900

    4.14
    주변 노드 출력 기능 추가.
    
    UI 손 봐야함.
    문자보내기 해야함.
    n초 마다 자기위치 계속 보내서 위험 알림 떠야함

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,9 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -16,7 +19,6 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
@@ -44,10 +46,16 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
     private String tMapKey = "l7xx0cbb43a68b824ac79560cdf4d46b8060";
 
     private boolean isDraw;
+    private boolean isNodeDraw;
+
     private ArrayList<WayPoint> m_mapPoint = new ArrayList<WayPoint>();
     private ArrayList<TMapPoint> points = new ArrayList<TMapPoint>();
 
+
+    private ArrayList<WayPoint> m_nodePoint = new ArrayList<WayPoint>();
+
     public static int safety = 0;
+    public static int node = 0;
 
     public static Context context;
     public static TMapView tMapView;
@@ -115,14 +123,13 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         DrawMarker();
     }
 
-    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener()
-    {
-        @Override public void onDrawerSlide(@NonNull View drawerView, float slideOffset) { }
-        @Override public void onDrawerOpened(@NonNull View drawerView) { }
-        @Override public void onDrawerClosed(@NonNull View drawerView) { }
-        @Override public void onDrawerStateChanged(int newState) { }
-    };
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
 
     @Override
     public void onLocationChange(Location location)
@@ -132,7 +139,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
         myPoint = new TMapPoint(location.getLongitude(), location.getLatitude());
     }
-
 
     void ShowMap( android.widget.LinearLayout linearLayoutTmap , com.skt.Tmap.TMapView tMapView)
     {
@@ -157,17 +163,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         Button buttonDefault = (Button)findViewById(R.id.buttonDefault);
         Button buttonAdvance = (Button)findViewById(R.id.buttonAdvance);
         Button buttonStart = (Button)findViewById(R.id.buttonStart);
-
-        Button buttonOpenSide = (Button)findViewById(R.id.buttonOpenSide);
-
-        //buttonOpenSide
-
-
-        Button btn_side_cctv;
-        Button btn_side_path;
-        Button btn_side_light;
-        Button btn_side_police;
-
 
         buttonDefault.setOnClickListener(new View.OnClickListener()
         {
@@ -206,26 +201,81 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
                 GetData mGetData = new GetData();
                 mGetData.start();
-
-                /*
-                FindPath mFindPath = new FindPath();
-                mFindPath.start();
-                 */
             }
         });
 
-        buttonOpenSide.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                //drawerLayout.openDrawer(drawerView);
-            }
-        });
-
-
-        //button
     }
+
+
+
+    ///////////////
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        GetNodeData mGetNodeData = new GetNodeData();
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                node = 1;
+                if(isNodeDraw == true)
+                    isNodeDraw = false;
+                else
+                    isNodeDraw = true;
+
+                mGetNodeData.start();
+
+                return true;
+
+
+            case R.id.item2:
+                node = 2;
+
+                if(isNodeDraw == true)
+                    isNodeDraw = false;
+                else
+                    isNodeDraw = true;
+
+                mGetNodeData.start();
+
+                return true;
+
+            case R.id.item3:
+                node = 3;
+
+                if(isNodeDraw == true)
+                    isNodeDraw = false;
+                else
+                    isNodeDraw = true;
+
+                mGetNodeData.start();
+                return true;
+
+
+            case R.id.item4:
+                node = 4;
+
+                if(isNodeDraw == true)
+                    isNodeDraw = false;
+                else
+                    isNodeDraw = true;
+
+                mGetNodeData.start();
+                return true;
+
+
+            case R.id.item5:
+                isNodeDraw = false;
+
+                NodeMarkerCon mNodeMarkerCon = new NodeMarkerCon();
+                mNodeMarkerCon.start();
+
+                return true;
+        }
+        return false;
+    }
+
+    ///////////////
 
     void RecievePoint() //시작점 끝점 찍혀있는 마크로 업데이트
     {
@@ -257,8 +307,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
                 Bitmap bitmapCCTV = BitmapFactory.decodeResource(context.getResources(),R.drawable.cctv);
                 tItem.setIcon(bitmapCCTV);
-
-
 
                 tMapView.addMarkerItem(m_mapPoint.get(i).getName(),tItem);
             }
@@ -345,8 +393,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         {
             try
             {
-                DrawMarker();
-
                 if(isDraw == true)
                 {
                     for (int i = 0 ; i < m_mapPoint.size() ; i++) //draw path
@@ -363,34 +409,9 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
                         //TMapPolyLine tMapPolyLine = tmapdata.findPathDataWithType(TMapPathType.CAR_PATH, point1, point2);
 
-                        Thread.sleep(500);
+                        Thread.sleep(450);
                     }
                 }
-
-/*
-                if(isDraw == true)
-                {
-                    for (int i = 1 ; i < m_mapPoint.size()-1 ; i++)
-                    {
-                        points.add(new TMapPoint(m_mapPoint.get(i).getLatitude() , m_mapPoint.get(i).getLongitude()));
-                    }
-
-                    TMapPoint tMapPointStart = new TMapPoint(m_mapPoint.get(0).getLatitude() , m_mapPoint.get(0).getLongitude());
-                    TMapPoint tMapPointEnd = new TMapPoint(m_mapPoint.get(m_mapPoint.size()).getLatitude() , m_mapPoint.get(m_mapPoint.size()).getLongitude());
-                    TMapPolyLine tMapPolyLine = new TMapData().findPathDataWithType
-                            (TMapData.TMapPathType.PEDESTRIAN_PATH,
-                                    tMapPointStart,
-                                    tMapPointEnd,
-                                    points,
-                                    0);
-
-
-                    tMapPolyLine.setLineColor(Color.BLUE);
-                    tMapPolyLine.setLineWidth(2);
-                    tMapView.addTMapPolyLine("DefaultName", tMapPolyLine);
-                }
-
- */
 
                 else
                 {
@@ -420,17 +441,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
                 m_mapPoint.clear();
                 RecievePoint(); //초기화.
 
-
-                /*
-                _url = "http://api.floodnut.com/safe/routing?" +
-                        "srcLati=" + m_mapPoint.get(0).getLatitude() +
-                        "&srcLongti=" + m_mapPoint.get(0).getLongitude() +
-                        "&dstLati=" + m_mapPoint.get(1).getLatitude() +
-                        "&dstLongti=" + m_mapPoint.get(1).getLatitude() +
-                        "&passList=&safeDegree=" + safety;
-
-                 */
-
                 Double sLa = m_mapPoint.get(0).getLatitude();
                 Double sLo = m_mapPoint.get(0).getLongitude();
                 Double dLa = m_mapPoint.get(1).getLatitude();
@@ -438,7 +448,7 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
 
                 //http://api.floodnut.com/safe/routing?srcLati=35.248687&srcLongti=128.682841&dstLati=35.237031&dstLongti=128.666649&passList=&safeDegree=1
-                _url = "http://api.floodnut.com/safe/routing?" +
+                _url =  "http://api.floodnut.com/safe/routing?" +
                         "srcLati="+  sLa +"&srcLongti="+ sLo +
                         "&dstLati=" + dLa + "&dstLongti=" + dLo +
                         "&passList=&safeDegree=" + safety;
@@ -481,7 +491,6 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
         }
     }
 
-
     public void parseJSON(String src)   //parsing
     {
         try {
@@ -498,6 +507,185 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
 
                 m_mapPoint.add( i+1, new WayPoint( name, Double.parseDouble(lati), Double.parseDouble(longti), Integer.parseInt(type) ) );
             }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /////////////////////////
+    //node
+
+    public class GetNodeData extends Thread
+    {
+        String _url;
+        String result;
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                m_nodePoint.clear();
+                RecievePoint(); //초기화.
+
+                Double sLa = m_mapPoint.get(0).getLatitude();
+                Double sLo = m_mapPoint.get(0).getLongitude();
+
+                //http://api.floodnut.com/safe/node?srcLati=35.248687&srcLongti=128.682841&mode=1
+                _url =  "http://api.floodnut.com/safe/node?" +
+                        "srcLati="+  sLa +"&srcLongti="+ sLo +
+                        "&mode=" + 1;
+
+                java.net.URL url = new URL(_url);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                if( connection != null)
+                {
+                    connection.setRequestMethod("GET");
+                    connection.setDoInput(true);
+
+                    InputStream is = connection.getInputStream();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+
+                    while((result = br.readLine())!=null)
+                    {
+                        sb.append(result+"\n");
+                    }
+
+                    result = sb.toString();
+                }
+
+                //parsing
+                parseJSON_(result);
+
+                NodeMarkerCon nodeMarkerCon = new NodeMarkerCon();
+                nodeMarkerCon.run();
+
+                Thread.interrupted();
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+    void DrawNodeMarker()
+    {
+        for (int i = 0 ; i < m_nodePoint.size() ; i++)
+        {
+            if(m_nodePoint.get(i).getType() == 1)
+            {
+                TMapPoint tMapPointMarker = new TMapPoint(m_nodePoint.get(i).getLatitude() , m_nodePoint.get(i).getLongitude());
+
+                TMapMarkerItem tItem = new TMapMarkerItem();
+
+                tItem.setTMapPoint(tMapPointMarker);
+                tItem.setName("node " + i);
+                tItem.setVisible(TMapMarkerItem.VISIBLE);
+
+                Bitmap bitmapCCTV = BitmapFactory.decodeResource(context.getResources(),R.drawable.cctv);
+                tItem.setIcon(bitmapCCTV);
+
+                tMapView.addMarkerItem(m_nodePoint.get(i).getName(),tItem);
+            }
+
+            else if(m_nodePoint.get(i).getType() == 2)
+            {
+                TMapPoint tMapPointMarker = new TMapPoint(m_nodePoint.get(i).getLatitude() , m_nodePoint.get(i).getLongitude());
+
+                TMapMarkerItem tItem = new TMapMarkerItem();
+
+                tItem.setTMapPoint(tMapPointMarker);
+                tItem.setName("point " + i);
+                tItem.setVisible(TMapMarkerItem.VISIBLE);
+
+                Bitmap bitmapCCTV = BitmapFactory.decodeResource(context.getResources(),R.drawable.pass);
+                tItem.setIcon(bitmapCCTV);
+
+                tMapView.addMarkerItem(m_nodePoint.get(i).getName(),tItem);
+            }
+
+            else if(m_nodePoint.get(i).getType() == 3)   //교통량
+            {
+                TMapPoint tMapPointMarker = new TMapPoint(m_nodePoint.get(i).getLatitude() , m_nodePoint.get(i).getLongitude());
+
+                TMapMarkerItem tItem = new TMapMarkerItem();
+
+                tItem.setTMapPoint(tMapPointMarker);
+                tItem.setName("point " + i);
+                tItem.setVisible(TMapMarkerItem.VISIBLE);
+
+                Bitmap bitmapPass = BitmapFactory.decodeResource(context.getResources(),R.drawable.pass);
+                tItem.setIcon(bitmapPass);
+
+                tItem.setPosition(-0.01f, 0.95f);         // 마커의 중심점을 하단, 중앙으로 설정
+
+                tMapView.addMarkerItem(m_nodePoint.get(i).getName(),tItem);
+            }
+
+            else if(m_nodePoint.get(i).getType() == 4)   //가로등
+            {
+                TMapPoint tMapPointMarker = new TMapPoint(m_nodePoint.get(i).getLatitude() , m_nodePoint.get(i).getLongitude());
+
+                TMapMarkerItem tItem = new TMapMarkerItem();
+
+                tItem.setTMapPoint(tMapPointMarker);
+                tItem.setName("point " + i);
+                tItem.setVisible(TMapMarkerItem.VISIBLE);
+
+                Bitmap bitmapLight = BitmapFactory.decodeResource(context.getResources(),R.drawable.light);
+                tItem.setIcon(bitmapLight);
+
+                tItem.setPosition(-0.01f, 0.95f);         // 마커의 중심점을 하단, 중앙으로 설정
+
+                tMapView.addMarkerItem(m_nodePoint.get(i).getName(),tItem);
+            }
+        }
+    }
+    public class NodeMarkerCon extends Thread
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                DrawNodeMarker();
+
+                if(isNodeDraw == false)
+                {
+                    m_nodePoint.clear();
+                    tMapView.removeAllMarkerItem();
+                }
+
+                Thread.interrupted();
+
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void parseJSON_(String src)   //parsing
+    {
+        try {
+            JSONObject root = (JSONObject) new JSONTokener(src).nextValue();
+
+            JSONArray array = new JSONArray(root.getString("data"));
+
+            for(int i = 0; i < array.length(); i++)
+            {
+                String name     = "point" + i + "th";
+                String lati     = array.getJSONObject(i).getString("lati");
+                String longti   = array.getJSONObject(i).getString("longi");
+                String type = array.getJSONObject(i).getString("type");
+
+                m_nodePoint.add( i, new WayPoint( name, Double.parseDouble(lati), Double.parseDouble(longti), Integer.parseInt(type) ) );
+            }
+
+            m_nodePoint.add(0,new WayPoint( "0000",myPoint.getLatitude(), myPoint.getLongitude(),4));
 
         } catch (JSONException e) {
             e.printStackTrace();
